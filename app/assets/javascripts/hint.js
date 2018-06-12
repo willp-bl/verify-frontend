cca68f095cba06ef0f5cb08ff9f244f6caa51e5b
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2018-06-12 10:17:11 +0100

    HUB-153: Renamed container names

--- app/assets/javascripts/hint.js
+++ app/assets/javascripts/hint.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-    $.get("http://localhost:50300/hint", function(data) {
+    $.get("https://www.signin.service.gov.uk/hint", function(data) {
         console.log(data);
         if(data.value){
         //if(true){
-            var e = $(".gem-c-radio:eq(1)");
-            //e.prev().insertAfter(e);
-            //$("#option-1").prop("checked", true);
-            e.css("background-color", "lightgreen");
-            $('#option-1').next().append(data.text)
-    }
+            var verifyRadio = $("input[value=sign-in-with-gov-uk-verify]")
+            var verifyContainer = verifyRadio.parent();
+            //verifyContainer.prev().insertAfter(verifyContainer);
+            //verifyRadio.prop("checked", true);
+            verifyContainer.css("background-color", "lightgreen");
+            verifyRadio.next().append(data.text)
+        }
     })
 });
